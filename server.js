5d47bd0536a4cb5d592a1f9e1b84c38b91c0453a
Chris_Wallace <kawallis@icloud.com>
2017-05-03 11:21:16 -0700

    added connection

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const app = require('./lib/app');
 const http = require('http');
 
-const connect = require('./lib/connect');
-const dbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/brews';
-connect(dbUri);
-
 const server = http.createServer(app);
 
 const port = process.env.PORT || 3000;
@@ -12,3 +8,18 @@ const port = process.env.PORT || 3000;
 server.listen(port, () => {
     console.log('server running on', server.address());
 });
+
+
+
+const mongoose = require('mongoose');
+mongoose.Promise = Promise;
+const dbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/brews';
+mongoose.connect(dbUri);
+
+process.on('SIGINT', function() {
+    mongoose.connection.close(function () {
+        console.log('You done close dat shieet');
+        process.exit(0);
+    });
+});
+
